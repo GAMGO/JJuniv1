83cc685f1c4b2966d8e97917bb272dadffce3b63
GAMGO <44716661+GAMGO@users.noreply.github.com>
2018-11-03 13:16:45 +0900

    Finite State Machine

---
+++ FSMManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum PlayerState
+{
+    IDLE = 0,
+    RUN,
+    CHASE,
+    ATTACK
+}
+public class FSMManager: MonoBehaviour {
+
+	void Start () {
+        
+	}
+	
+	void Update () {
+	    
+   	}
+}
